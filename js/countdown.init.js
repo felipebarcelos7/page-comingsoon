5eeef6d9287ee053953bbdc0b9c6e5a673fde179
Felipe Barcelos <75994694+felipebarcelos7@users.noreply.github.com>
2022-11-08 10:28:11 -0300

    Update countdown.init.js

--- js/countdown.init.js
+++ js/countdown.init.js
@@ -1,18 +1,18 @@
 //countdown.init.js
 $('#countdown').countdown('2022/11/30', function(event) {
 var $this = $(this).html(event.strftime(''
-    + '<div class="count-down"> <span class="count-number">%D</span> <span class="count-head position-relative d-block">Days</span></div> '
-    + '<div class="count-down"> <span class="count-number">%H</span> <span class="count-head position-relative d-block">Hours</span></div> '
-    + '<div class="count-down"> <span class="count-number">%M</span> <span class="count-head position-relative d-block">Minutes</span></div> '
-    + '<div class="count-down"> <span class="count-number">%S</span> <span class="count-head position-relative d-block">Seconds</span></div>'
+    + '<div class="count-down"> <span class="count-number">%D</span> <span class="count-head position-relative d-block">Dias</span></div> '
+    + '<div class="count-down"> <span class="count-number">%H</span> <span class="count-head position-relative d-block">Horas</span></div> '
+    + '<div class="count-down"> <span class="count-number">%M</span> <span class="count-head position-relative d-block">Minutos</span></div> '
+    + '<div class="count-down"> <span class="count-number">%S</span> <span class="count-head position-relative d-block">Segundis</span></div>'
     ));
 });
 
 $('#eventdown').countdown('2022/11/30', function(event) {
 var $this = $(this).html(event.strftime(''
-    + '<div class="count-down"> <span class="count-number">%D</span> <span class="count-head position-relative d-block">Days</span></div> '
-    + '<div class="count-down"> <span class="count-number">%H</span> <span class="count-head position-relative d-block">Hours</span></div> '
-    + '<div class="count-down"> <span class="count-number">%M</span> <span class="count-head position-relative d-block">Minutes</span></div> '
-    + '<div class="count-down"> <span class="count-number">%S</span> <span class="count-head position-relative d-block">Seconds</span></div>'
+    + '<div class="count-down"> <span class="count-number">%D</span> <span class="count-head position-relative d-block">Dias</span></div> '
+    + '<div class="count-down"> <span class="count-number">%H</span> <span class="count-head position-relative d-block">Horas</span></div> '
+    + '<div class="count-down"> <span class="count-number">%M</span> <span class="count-head position-relative d-block">Minutos</span></div> '
+    + '<div class="count-down"> <span class="count-number">%S</span> <span class="count-head position-relative d-block">Segundos</span></div>'
     ));
 });
